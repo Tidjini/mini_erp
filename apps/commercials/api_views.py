26210c8e6d7e4b731e4c2b50f8f4633b6475f349
Tidjini <tidjini.messaoudi@gmail.com>
2022-12-06 14:20:57 +0100

    order by id

--- apps/commercials/api_views.py
+++ apps/commercials/api_views.py
@@ -6,6 +6,6 @@ from . import serializers
 
 class ProductApiViewSet(viewsets.ModelViewSet):
 
-    queryset = models.Product.objects.all()
+    queryset = models.Product.objects.order_by('-id')
     serializer_class = serializers.ProductSerializer
     permission_classes = permissions.IsAuthenticated,
