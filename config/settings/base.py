ea896aeac6d3fbf985e17c727842ea4802f2c6c8
Tidjini <tidjini.messaoudi@gmail.com>
2022-12-06 10:12:11 +0100

    set dev database

--- config/settings/base.py
+++ config/settings/base.py
@@ -76,25 +76,6 @@ TEMPLATES = [
 WSGI_APPLICATION = "config.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": BASE_DIR / "db.sqlite3",
-    # },
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "messages",
-        "USER": "postgres",
-        "PASSWORD": 'user',
-        "HOST": "localhost",
-        "PORT": "",
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
--- config/settings/development.py
+++ config/settings/development.py
@@ -1,3 +1,20 @@
 from .base import *
 
 DEBUG = True
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # },
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "promag_groupe_dev",
+        "USER": "postgres",
+        "PASSWORD": 'user',
+        "HOST": "localhost",
+        "PORT": "",
+    }
+}
