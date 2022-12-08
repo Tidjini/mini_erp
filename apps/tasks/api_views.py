c18e05f33daaec7cae0449a189374bf28dce29ae
Tidjini <tidjini.messaoudi@gmail.com>
2022-12-08 09:32:03 +0100

    set Task Creator from tokenb

--- apps/tasks/api_views.py
+++ apps/tasks/api_views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
 
 from . import models, serializers
 
@@ -9,5 +10,13 @@ class TaskApiViewSet(viewsets.ModelViewSet):
 
     queryset = models.Task.objects.order_by('-created_at')
     serializer_class = serializers.TaskSerialzer
-    permission_classes = permissions.IsAuthenticated
+    permission_classes = permissions.IsAuthenticated,
 
+    def create(self, request, *args, **kwargs):
+
+        creator = request.user
+        if not creator:
+            return RecursionError
+        request.data.update({'creator': request.user.id})
+
+        return super().create(request, *args, **kwargs)
