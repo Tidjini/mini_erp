a68f00a821f2ca5a6b3d8a1746a53e664ad8ab10
Tidjini <tidjini.messaoudi@gmail.com>
2022-12-11 12:27:09 +0100

    filter by category

--- apps/stock/api_views.py
+++ apps/stock/api_views.py
@@ -1,10 +1,13 @@
 from rest_framework import viewsets, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 
 from . import models, serializers
 
 
 class UniteApiViewSet(viewsets.ModelViewSet):
 
-    queryset = models.Unite.objects.all()
+    queryset = models.Unite.objects.order_by('id')
     serializer_class = serializers.UniteSerializer
     permission_classes = permissions.IsAuthenticated,
+    filter_backends = DjangoFilterBackend,
+    filterset_fields = 'category',
