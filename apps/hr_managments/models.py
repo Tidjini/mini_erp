bda47298a185f9a4683faefd8e6f1e593d3aee58
Tidjini <tidjini.messaoudi@gmail.com>
2022-12-18 12:03:15 +0100

    Add User Localisation

--- apps/hr_managments/models.py
+++ apps/hr_managments/models.py
@@ -3,7 +3,22 @@ from apps.application.models import UtilsMixin
 from apps.general.models import Profile
 
 
-# class Employe(UtilsMixin):
+class Employe(UtilsMixin):
+
+    TYPES = (
+        ('C', 'CHAUFFEUR'),
+        ('A', 'ADMINISTRATION'),
+        ('O', 'OTHER'),
+    )
+    user = models.OneToOneField(
+        Profile, null=True, blank=True, on_delete=models.SET_NULL)
+    type = models.CharField(max_length=2, choices=TYPES, default='O')
 
 
-#     user = models.OneToOneField(Ut)
+class Localisation(UtilsMixin):
+
+    user = models.OneToOneField(
+        Profile, primary_key=True, on_delete=models.CASCADE)
+    longitude = models.DecimalField(
+        max_digits=50, decimal_places=20, default=0)
+    latitude = models.DecimalField(max_digits=50, decimal_places=20, default=0)
