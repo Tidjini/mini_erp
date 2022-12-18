c2b12a632f44a726ffa74194a366c06b822de508
Tidjini <tidjini.messaoudi@gmail.com>
2022-12-18 12:08:40 +0100

    set Profile to tasks

--- apps/tasks/models.py
+++ apps/tasks/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-from apps.chats.models import Utilisateur
-from apps.application.models import UtilsMixin
+from apps.general.models import Profile
 
 
 class Task(models.Model):
@@ -12,9 +11,9 @@ class Task(models.Model):
         ('c', 'canceled')
     )
     creator = models.ForeignKey(
-        Utilisateur, on_delete=models.CASCADE, related_name='created_tasks')
+        Profile, on_delete=models.CASCADE, related_name='created_tasks')
     receiver = models.ForeignKey(
-        Utilisateur, on_delete=models.SET_NULL, null=True,
+        Profile, on_delete=models.SET_NULL, null=True,
         related_name='tasks')
 
     statue = models.CharField(max_length=1, choices=STATUES, default='i')
