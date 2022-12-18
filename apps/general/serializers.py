861ef41b73635bf088e0c5151b06a6feed9fc873
Tidjini <tidjini.messaoudi@gmail.com>
2022-12-18 12:13:04 +0100

    Add Profile Serializer

--- apps/general/serializers.py
+++ apps/general/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
+
+from apps.application.serialzers import ModelSerializerMixin
 from . import models
 
 
@@ -9,3 +12,36 @@ class TvaSerializer(serializers.ModelSerializer):
         model = models.Tva
         fields = '__all__'
         read_only_fields = 'id',
+
+
+class ProfileSerializer(ModelSerializerMixin):
+    name = serializers.ReadOnlyField()
+    password_one = serializers.CharField(
+        style={"input_type": "password"}, write_only=True
+    )
+    password_two = serializers.CharField(
+        style={"input_type": "password"}, write_only=True
+    )
+
+    def save(self, **kwargs):
+        pwd_one, pwd_two = [
+            value
+            for key, value in self.validated_data.items()
+            if key in ("password_one", "password_two")
+        ]
+        # check passwords confromity
+        if pwd_one != pwd_two:
+            raise ValidationError("Passwords not matched, retry again")
+
+        self.clean_validate_data()
+        self._validated_data.update(password=pwd_one)
+
+        try:
+            return super().save(**kwargs)
+        except ValueError as e:
+            raise ValidationError(f'Exception du to: {e}')
+
+    class Meta:
+        model = models.Profile
+        exclude = ("password",)
+        read_only_fields = ("id",)
